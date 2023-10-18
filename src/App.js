4f4c5dd6e4c11e89f85aa7e9f8337597e875b257
vishwajeet <vishwajeets148@gmail.com>
2023-10-18 19:27:03 +0530

    Inshort Data Upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}></Route>
+
+
+    </Routes>
+    
+    
+    </BrowserRouter>
+    
+    
+    </>
+  )
+}
+
+export default App
